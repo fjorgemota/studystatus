cb3d5193fc3d0b2122b134bce37652e12a5a0184
CaiqueMarques <caiquermarques95@gmail.com>
2015-03-25 14:10:17 -0300

    Refactoring the size of title and description in task cards

--- src/views/TaskView.java
+++ src/views/TaskView.java
@@ -20,8 +20,9 @@ public class TaskView extends RenderableView {
         GroupLayout.SequentialGroup seqv = layout.createSequentialGroup();
         GroupLayout.ParallelGroup seqh = layout.createParallelGroup();
         JLabel title = new JLabel();
-        title.setText(this.task.getTitle());
-        title.setFont(title.getFont().deriveFont(Font.BOLD, 36));
+        String ts = this.task.getTitle();
+        title.setText((ts.length() > 17) ? ts.substring(0, 17)+"..." : ts);
+        title.setFont(title.getFont().deriveFont(Font.BOLD, 24));
         title.setBounds(HORIZONTAL_MARGIN, 5, this.getWidth() - (HORIZONTAL_MARGIN * 2), 30);
         //constraints.gridy = 0;
         seqv.addComponent(title, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE,
@@ -30,7 +31,8 @@ public class TaskView extends RenderableView {
                 GroupLayout.PREFERRED_SIZE);
         JLabel description = new JLabel();
         String ds = this.task.getDescription();
-        description.setText((ds.length() > 30) ? ds.substring(0, 30)+"...": ds);
+        description.setText((ds.length() > 28) ? ds.substring(0, 28)+"...": ds);
+        description.setFont(description.getFont().deriveFont(Font.BOLD, 14));
         description.setBounds(HORIZONTAL_MARGIN, 25, this.getWidth()-(HORIZONTAL_MARGIN*2), 30);
         //constraints.gridy = 1;
         seqv.addComponent(description, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE,
